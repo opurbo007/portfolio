2b90bfb2348b60e5e6813949377acd5c670f70f8
Opurbo007 <opurboujjol@gmail.com>
2026-09-02 21:50:47 +0600

    refactor Home component: reorganize imports and simplify project rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,27 @@
-import About from "@/components/About";
-import Contact from "@/components/Contact";
 import { HeroSection } from "@/components/HeroSection";
-import Project from "@/components/Project";
+import About from "@/components/About";
 import Skill from "@/components/Skill";
-
-interface Project {
-  _id: number;
-  name: string;
-  git: string;
-  live?: string;
-  imagePath: string;
-}
-const projectData: Project[] = [
-  {
-    _id: 1,
-    name: "ShowBox",
-    git: "https://github.com/opurbo007/showbox",
-    imagePath: "/temp/project13.png",
-    live: "https://showbox-two.vercel.app/",
-  },
-  {
-    _id: 2,
-    name: "Portfolio",
-    git: "https://github.com/opurbo007/portfolio",
-    live: "https://portfolio-theta-hazel-45.vercel.app/",
-    imagePath: "/temp/project1.png",
-  },
-  {
-    _id: 3,
-    name: "Qoo-Media",
-    git: "https://github.com/opurbo007/Qoo-media",
-    live: "https://qoo-media.vercel.app/",
-    imagePath: "/temp/project2.png",
-  },
-];
+import Project from "@/components/Project";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
-    <>
-      <div className="overflow-hidden">
+    <div className="overflow-hidden">
+      <section id="home">
         <HeroSection />
+      </section>
+      <section id="about">
         <About />
+      </section>
+      <section id="skills">
         <Skill />
-        <Project projectData={projectData} />
+      </section>
+      <section id="projects">
+        <Project />
+      </section>
+      <section id="contact">
         <Contact />
-      </div>
-    </>
+      </section>
+    </div>
   );
 }
